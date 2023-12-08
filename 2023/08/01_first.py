ebe605ba5e430b6d4ead7ffc5077a840fdc33611
Omar Hermannsson <omar@generic.is>
2023-12-08 13:45:46 +0000

    Solved 08 - part1

--- 2023/08/01_first.py
+++ 2023/08/01_first.py
@@ -4,7 +4,7 @@ import os
 script_path = os.path.dirname(os.path.abspath(__file__))
 
 filename = '_sample.txt'
-#filename = '_input.txt'
+filename = '_input.txt'
 
 input_file = open(os.path.join(script_path, filename))
 
@@ -13,39 +13,47 @@ element = None
 class Element:
     def __init__(self, element_str):
         self.id = element_str[:3]
-        self.left, self.right = element_str[6:].strip('()').replace(' ','').split(',')
+        self.left, self.right = element_str[6:].strip('()\n').replace(' ','').split(',')
     def __repr__(self):
         return self.id
     def __str__(self):
         return self.id
+    def get_next(self, token):
+        if token == 'L':
+            return self.left
+        if token == 'R':
+            return self.right
+        else:
+            raise ValueError(f"{token} invalid value")
 
 
 element_table = {}
 
 for i, line in enumerate(input_file):
     if i == 0:
-        instructions = list(line)
-    elif i > 1:
+        instructions = list(line.strip())
+    elif i > 1 and len(line)>1:
         element = Element(line)
         element_table[element.id] = element
     
     if i == 2:
         first = element.id
-    
-print(element_table)
 
+    #if element is not None and (element.id == element.left == element.right):
+    #    print ("LAST?:", element)
+    
 cnt = 0
 mod = len(instructions)
-next = first
+next = 'AAA'
+last = 'ZZZ'
 
-print ("First:", first)
 while True:
     instruction = instructions[cnt%mod]
-    next = element_table[str(next)]
+    next = element_table[next].get_next(instruction)
     cnt += 1
-    if str(next) == 'ZZZ':
+    #if cnt%10000 == 0:
+    #    print("Count:", cnt, " - instruction:", instruction, " - mod:", cnt%mod, " - next:", next, " - last:", last)
+    if next == last:
         break
-    #print(next,end=', ')
-
 
 print("Steps:", cnt)
