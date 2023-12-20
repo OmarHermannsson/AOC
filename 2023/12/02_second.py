81b230cfba33787172fd58889c465d9f25548eee
Omar Hermannsson <omar@generic.is>
2023-12-20 11:18:31 +0000

    12 - part 2 solved
    Got a little help and revised my solution to use a recursive function and memorizing operations

--- 2023/12/02_second.py
+++ 2023/12/02_second.py
@@ -7,65 +7,39 @@ filename = '_input.txt'
 
 input_file = open(os.path.join(script_path, filename))
 
-def getCharPower(stringLength, charRange):
-    charpowers = []
-    for x in range(0, stringLength):
-            charpowers.append(len(charRange)**(stringLength - x - 1))
-    return charpowers
+cached = {}
 
-def Generator(stringLength, charRange):
-    workbench = []
-    results = []
-    charpowers = getCharPower(stringLength, charRange)
-    for x in range(0, stringLength):
-            while len(workbench) < len(charRange)**stringLength:
-                    for char in charRange:
-                            for z in range(0, charpowers[x]):
-                                    workbench.append(char)
-            results.append(workbench)
-            workbench = []
-    results = ["".join(result) for result in list(zip(*results))]
-    return results
+def get_count(rec, par):        
+    if rec == "":
+        return 1 if par == () else 0
+    
+    if par == ():
+        return 0 if "#" in rec else 1
+    
+    key = (rec,par)
 
-def get_combinations(num):
-    return Generator(num, '.#')
-
-def get_counts(rec):
-    counts = []
+    if key in cached:
+        return cached[key]
+    
     count = 0
-    for x, char in enumerate(rec):
-        if char == '#':
-            count += 1
-        elif char == '.':
-            if rec[max(0,x-1)] == '#':
-                counts.append(count)
-            count = 0
-    if count > 0:
-         counts.append(count)
-    count_str = ','.join(map(str,counts))
-    return count_str
 
-def get_possible(rec, par):
-    arrangements = 0
-    possible = []
-    lrec = list(rec)
-    wildcards = [i for i,x in enumerate(lrec) if x=='?']
-    combinations = get_combinations(len(wildcards))
-    for c in combinations:
-        for i, w in enumerate(wildcards):
-            lrec[w] = c[i]
-        if get_counts(lrec) == par:
-            possible.append(lrec.copy())
-            arrangements += 1
-    return possible, arrangements
-            
+    if rec[0] in ".?":
+        count += get_count(rec[1:], par)
+
+    if rec[0] in "#?":
+        if par[0] <= len(rec) and "." not in rec[:par[0]] and (par[0] == len(rec) or rec[par[0]] != "#"):
+            count += get_count(rec[par[0] + 1:], par[1:])
+    
+    cached[key] = count
+    return count
 
 sum = 0
 for line in input_file:
     rec, par = line.strip().split()
+    par = tuple(map(int, par.split(",")))
     frec = ((rec + '?')*5)[:-1]
-    fpar =  ((par + ',')*5)[:-1]
-    all, num = get_possible(frec, fpar)
+    fpar = par * 5
+    num = get_count(frec, fpar)
     sum += num
 
 print ("Num:", sum)
