e79c78c96141b5a0335405953d16ae9231e4bb91
OmarHermannsson <omar@generic.is>
2023-12-21 18:45:56 +0000

    13 - part 2 solved
    Had to go into some deep debugging to find what was wrong with the code but I eventually found it

--- 2023/13/02_second.py
+++ 2023/13/02_second.py
@@ -7,7 +7,6 @@ filename = '_input.txt'
 
 input_file = open(os.path.join(script_path, filename))
 
-
 patterns = []
 pattern = []
 
@@ -19,8 +18,9 @@ for line in input_file:
         pattern.append(line.strip())
 patterns.append(pattern.copy())
 
-def find_h_line(pattern):
+def find_h_line(pattern, ignore=None):
     l0 = len(pattern[0])
+    pattern = list(map(list,pattern))
     candidates = dict.fromkeys(range(l0), 0)
     for line in pattern:        
         for x, _ in enumerate(line):
@@ -29,25 +29,25 @@ def find_h_line(pattern):
             l1 = len(line[:x])
             l2 = len(line[x:])
             ml = min(l1,l2)
-            if line[x-ml:x] == line[x:ml+x][::-1]:
-                #print("M:", line, x+1)
+            if line[x-ml:x] == list(reversed(line[x:x+ml])) and x != ignore:
                 candidates[x] += 1
     for c in candidates:
         if candidates[c] == len(pattern):
-            #print("V:", c)
             return c
     return 0
 
-def find_v_line(pattern):
+def find_v_line(pattern, ignore=None):
     l0 = len(pattern)
+    if ignore is not None and ignore >= 100:
+        ignore = int(ignore / 100)
+    pattern = list(map(list,pattern))
     for x, _ in enumerate(pattern):
         if x == 0:
             continue
         l1 = len(pattern[:x])
         l2 = len(pattern[x:])
         ml = min(l1,l2)
-        if pattern[x-ml:x] == pattern[x:ml+x][::-1]:
-            #print("H:", x)
+        if pattern[x-ml:x] == list(reversed(pattern[x:x+ml])) and x != ignore:
             return 100*x
     return 0
 
@@ -59,11 +59,10 @@ def find_line(pattern):
     times = 0
     for y, line in enumerate(pattern):
         for x, _ in enumerate(line):
-            #new_pattern = pattern.copy()
             new_pattern = list(map(list, pattern))
             new_pattern[y][x] = "." if pattern[y][x] == "#" else "#"
-            new_hsum = find_h_line(new_pattern)
-            new_vsum = find_v_line(new_pattern)
+            new_hsum = find_h_line(new_pattern, old_hsum)
+            new_vsum = find_v_line(new_pattern, old_vsum)
             if new_hsum != old_hsum and new_hsum > 0:
                 sum += new_hsum
                 times += 1
@@ -71,28 +70,10 @@ def find_line(pattern):
             if new_vsum != old_vsum and new_vsum > 0:
                 new_vsum_dict[int(new_vsum/100)] = True
                 return new_vsum
-                #sum += new_vsum
-                #times += 1
-            #sum += new_hsum
-            #sum += new_vsum
-            #if sum > 0:
-            #    return sum
-    
-    if len(new_vsum_dict) == 1:
-        sum += next(iter(new_vsum_dict))
-        times += 1
-
-    if times != 1:
-        print("Times:", times, pattern, new_vsum_dict.keys(), old_hsum+old_vsum)
-    if len(new_vsum_dict.keys()) > 1:
-        print("Dict:", times, pattern, new_vsum_dict.keys())
-    return old_hsum+old_vsum
+    return 0
 
 sum = 0
 for pattern in patterns:
-    #print("Pattern:", pattern)
     sum += find_line(pattern)
     
 print("Sum:", sum)
-
-#42996
